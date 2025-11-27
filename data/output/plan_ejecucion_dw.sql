d5f1641dbd66273a3c16a221931bc9b90ac96b23
Alex Guaman <kennypallchizaca@gmail.com>
2025-11-26 20:15:11 -0500

    agregando tablas sql

--- data/output/plan_ejecucion_dw.sql
+++ data/output/plan_ejecucion_dw.sql
@@ -4,6 +4,7 @@ SET ECHO ON;
 SET FEEDBACK ON;
 SET SERVEROUTPUT ON;
 WHENEVER SQLERROR CONTINUE;
+--SPOOL data/output/plan_ejecucion_dw.log REPLACE;
 -- Si las tablas base estan en otro esquema, descomenta y ajusta:
 -- ALTER SESSION SET CURRENT_SCHEMA=ESQUEMAORIGINAL;
 @scripts/sql/oltp/00_create_base_tables.sql
@@ -14,7 +15,6 @@ WHENEVER SQLERROR CONTINUE;
 @scripts/sql/oltp/02_add_ciudad_to_clientes.sql
 @scripts/sql/oltp/03_assign_random_city_to_clients.sql
 @scripts/sql/oltp/04_create_province_canton_parish_tables.sql
-@data/output/jerarquia/insert_jerarquia.sql
 @scripts/sql/dw/01_dw_star_schema_and_top_product_view.sql
 @scripts/sql/etl/load_dw_from_oltp.sql
 
@@ -27,3 +27,4 @@ PROMPT Conteo en DW_DIM_UBICACION:
 SELECT COUNT(*) AS TOTAL_DIM_UBICACION FROM DW_DIM_UBICACION;
 PROMPT Top producto mas vendido (si hay datos):
 SELECT * FROM VW_MAS_VENDIDO WHERE ROWNUM <= 5;
+SPOOL OFF;
